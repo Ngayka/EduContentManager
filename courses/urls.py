e74cbf9d72898b984c11f4bb1536952160449b4d
Ngayka <ngayka@gmail.com>
2025-05-20 15:57:14 +0300

    html boostrap files

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from courses.views import index
-
+from courses.views import index, TeacherListView, TeacherDetailView
 
 app_name = 'courses'
 urlpatterns = [
     path('courses/', index, name='index'),
+    path("teachers", TeacherListView.as_view(), name='teacher-list'),
+    path("teachers/<int:pk>", TeacherDetailView.as_view(), name='teacher-detail'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
 from courses.models import Teacher, Course
 
 
@@ -15,4 +17,17 @@ def index(request):
                "num_visits": num_visits + 1,}
 
     return render(request, "courses/index.html", context=context)
+
+class TeacherListView(ListView):
+    model = Teacher
+    context_object_name = "teachers"
+    paginate_by = 5
+
+class TeacherDetailView(DetailView):
+    model = Teacher
+
+class CoursesListView(ListView):
+    model = Course
+    context_object_name = "courses"
+
 
