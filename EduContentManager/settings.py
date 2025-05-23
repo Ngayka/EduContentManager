795dba0218c67d01f904e30c44e6d09687f143f4
Ngayka <ngayka@gmail.com>
2025-05-23 19:30:29 +0300

    update settings

--- EduContentManager/settings.py
+++ EduContentManager/settings.py
@@ -25,12 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-+go3tc%@lhht!=hg4p6y*2%t7yx2np%&kz27cz7^pph*6a%8rx")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "FALSE"
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
