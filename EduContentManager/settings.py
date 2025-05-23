672653e3938b7582e7830a86edf7cd1142da75a3
Ngayka <ngayka@gmail.com>
2025-05-23 17:57:40 +0300

    added more information to READme file about project. Store SECRET_KEY in .env to prevent exposure in source code

--- EduContentManager/settings.py
+++ EduContentManager/settings.py
@@ -9,10 +9,13 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 from django.urls import reverse_lazy
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+go3tc%@lhht!=hg4p6y*2%t7yx2np%&kz27cz7^pph*6a%8rx"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
